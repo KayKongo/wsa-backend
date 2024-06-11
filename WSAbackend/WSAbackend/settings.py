a659606a628a2f309111de48fe7bcdcbc8615e54
kaykongo <pkkongo@st.ug.edu.gh>
2024-06-11 19:46:36 +0000

    chore: migrated Postgres database to cloud on render

--- WSAbackend/WSAbackend/settings.py
+++ WSAbackend/WSAbackend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,10 +89,7 @@ WSGI_APPLICATION = 'WSAbackend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://ecommerce_db_ku0a_user:Lw8BVD43L3Z4z01U8YjYMuSddjFjVBaQ@dpg-cpk6hqvsc6pc73enkt90-a.oregon-postgres.render.com/ecommerce_db_ku0a')
 }
 
 
